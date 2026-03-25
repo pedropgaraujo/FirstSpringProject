d1c9c30fd03dcf55a966e24aac5ebfbd43719d5e
Pedro Araujo <pedroaraujo.cdc@gmail.com>
2026-03-25 19:38:39 +0000

    Dados Temporada e Episodio

--- src/main/java/br/com/pedropgaraujo/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/pedropgaraujo/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.pedropgaraujo.screenmatch;
 
-import br.com.pedropgaraujo.screenmatch.model.DadosSerie;
-import br.com.pedropgaraujo.screenmatch.service.ConsumoAPI;
-import br.com.pedropgaraujo.screenmatch.service.ConverteDados;
+import br.com.pedropgaraujo.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoAPI();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=b09d932f");
-//        System.out.println(json);
-//        json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
-
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
-        System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
     }
 }
